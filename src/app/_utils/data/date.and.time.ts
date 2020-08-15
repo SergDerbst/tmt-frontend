aff1be07da84d6e3cb94f64bcce6678f9655eada
SergDerbst <sergio.weigel@gmail.com>
2020-08-15 18:05:50 +0200

    fixed stupid refactoring errors in auth module

--- src/app/_utils/data/date.and.time.ts
+++ src/app/_utils/data/date.and.time.ts
@@ -6,7 +6,7 @@ import {DateTimeUnit} from "./enums";
 export class DateFormatMap extends Map<string, { format: string, separator: string, regexSep: RegExp }> {
 	constructor() {
 		super();
-		this.set('en-US', { format: 'MM/dd/yyyy', separator: '/', regexSep: /\//g });
+		this.set('en-US', { format: 'MM/DD/YYYY', separator: '/', regexSep: /\//g });
 	}
 }
 
